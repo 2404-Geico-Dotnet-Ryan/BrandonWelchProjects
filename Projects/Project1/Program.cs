78d1275e5fea2f1740bf118a64e3204914b24343
Brandon-Welch <bwelch3800@hotmail.com>
2024-05-06 12:55:22 -0400

    Project Proposal

---
+++ Projects/Project1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{//---Start of Class Program---
+    static void Main(string[] args)
+    {//---Start of Main Method---
+
+       // main method code here 
+
+    }//---End of Main Method---
+}//---End of Class Program---
