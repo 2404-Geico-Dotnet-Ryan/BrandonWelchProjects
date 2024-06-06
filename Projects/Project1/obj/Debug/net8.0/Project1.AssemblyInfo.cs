608e4a28ed9dc3b2eacd79979a16ffc18f5401a3
Brandon-Welch <bwelch3800@hotmail.com>
2024-06-06 08:25:04 -0400

    updated PetController, added PetService, added IPetService

--- Projects/Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
+++ Projects/Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81881634ce8dabbaa3105e0ad7fd0ffb67ae9346")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9a9636c4029354a00248b52cb209fa3776d1ef7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
